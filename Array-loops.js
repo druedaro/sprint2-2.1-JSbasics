dce1cf4a9b109d09af31ad37436870e2aedbd284
David <ruedarosasdavid@gmail.com>
2025-06-11 12:52:21 +0200

    array-loops WIP

--- Array-loops.js
+++ Array-loops.js
@@ -4,9 +4,17 @@
     // Nivell 1
         // Exercici 1
         // forEach: Teniu una array de noms. Utilitza forEach per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+        let noms = ['Anna', 'Bernat', 'Clara'];
+
+        console.log('Exercici 1: ');
+        noms.forEach(element => console.log(element))
 
         // Exercici 2
         // for-of: Teniu una array de noms. Utilitza un bucle for-of per a imprimir cada nom a la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+        console.log('Exercici 2: ');
+        for (let nom of noms) {
+            console.log(nom);
+        }
 
         // Exercici 3
         // filter: Teniu una array de números. Utilitza filter per a crear una nova array que només contingui els números parells. let numeros = [1, 2, 3, 4, 5, 6];
