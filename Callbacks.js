8c7086ed5993b3ebff62a82c7aaf920e3db2889b
David <ruedarosasdavid@gmail.com>
2025-06-11 09:44:56 +0200

    operador-ternari-finished

--- Callbacks.js
+++ Callbacks.js
@@ -3,19 +3,46 @@
     // Nivell 1
         // Exercici 1
         // Callback bàsic: Escriu una funció anomenada processar que accepti dos paràmetres: un nombre i una funció de callback. La funció processar ha d'invocar la funció de callback, passant el nombre com a paràmetre.
+        let processar = (nombre, callback) => callback (nombre)
+        let greetings = nombre => console.log(`Hola ${nombre}`);
+
+        processar ("David", greetings);
 
         // Exercici 2
         // Callbacks amb operacions matemàtiques: Escriu una funció calculadora que accepti tres paràmetres: dos nombres i una funció de callback. La funció calculadora ha d'invocar la funció de callback amb els dos nombres com a paràmetres. Després, crida calculadora amb una funció que faci la suma dels dos nombres.
+        let calculadora = (num1, num2, callback) => callback (num1, num2)
+
+        console.log(calculadora (1,2, (a,b) => a+b));
 
 
     // Nivell 2
         // Exercici 3
         // Ús de callbacks en funcions asíncrones: Escriu una funció esperarISaludar que accepti dos paràmetres: un nom i una funció de callback. La funció ha d'esperar 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
+        let esperarISaludar = (nom, callback) => setTimeout(() => callback(nom), 2000)
+
+        esperarISaludar('David tornarà en 2 segons', (message) => console.log(message));
 
         // Exercici 4
         // Callbacks amb arrays: Escriu una funció processarElements que accepti dos paràmetres: un array i una funció de callback. La funció processarElements ha d'invocar la funció de callback per cada element de l'array.
+        const names = ["David", "Sara", "Alex", "Anna"]
+
+        function processarElements(array, callback) {
+        for (let i = 0; i < array.length; i++) {
+            callback(array[i]);
+        }
+        }
+
+        processarElements(names, (name) => {
+        console.log(`Bon dia, ${name}!`);
+        });
+
+
 
 
     // Nivell 3
         // Exercici 5
         // Escriu una funció processarCadena que accepti dos paràmetres: una cadena de caràcters i una funció de callback. La funció processarCadena ha de convertir la cadena a majúscules i llavors invocar la funció de callback amb la cadena transformada.
+        let processarCadena = (chain, callback) => callback (chain.toUpperCase())
+
+        processarCadena ('David té una PS5', (message) => console.log(message))
+        
